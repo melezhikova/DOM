c2e0b15da8b900072d155781ea9d63df08eb45c9
Liudmila Melezhikova <melezhikova@yandex.ru>
2021-10-19 10:15:49 +0500

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -3,37 +3,35 @@ const boardSize = 4;
 const container = document.getElementById('game-container');
 
 if (container) {
-    for (let i = 0; i < boardSize ** 2; i += 1) {
+  for (let i = 0; i < boardSize ** 2; i += 1) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     container.appendChild(cell);
-    }
+  }
 }
 
 const cells = Array.from(document.querySelectorAll('.cell'));
 
-function figaro (index, number) {
-    if (index !== -1) {
-        cells[index].classList.remove('character');
-    }
-    const figaroCell = cells[number];
-    figaroCell.classList.add('character');
+function figaro(index, number) {
+  if (index !== -1) {
+    cells[index].classList.remove('character');
+  }
+  const figaroCell = cells[number];
+  figaroCell.classList.add('character');
 }
 
-setInterval (() => {
-    const index = cells.findIndex((item) => {
-        return item.className.includes('character');
-    })
-    let number = Math.floor(Math.random() * cells.length);
-    if (number === index) {
-        number = Math.floor(Math.random() * cells.length);
-    } else {
-        figaro (index, number);
-    }
-}
-, 1500);
+setInterval(() => {
+  const index = cells.findIndex((item) => item.className.includes('character'));
+  let number = Math.floor(Math.random() * cells.length);
+  if (number === index) {
+    number = Math.floor(Math.random() * cells.length);
+  } else {
+    figaro(index, number);
+  }
+},
+1500);
 
-//функция для теста, чтобы не ругался circleci
+// функция для теста, чтобы не ругался circleci
 export default function demo(value) {
-    return value;
+  return value;
 }
